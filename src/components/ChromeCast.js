5795e5982b67713aca120dc6591fa14a1099cc19
Noah Schwagele <42180575+noahschwagele@users.noreply.github.com>
2024-05-15 11:59:08 +0200

    Custom Channel Uploading

--- src/components/ChromeCast.js
+++ src/components/ChromeCast.js
@@ -1,34 +1,48 @@
 import React, { useEffect } from 'react';
-import { CastButton, useRemoteMediaClient, useCastChannel } from 'react-native-google-cast';
+import { CastButton, useRemoteMediaClient, useCastChannel, useCastSession} from 'react-native-google-cast';
+
 
 export default function ChromeCast() {
-  const channel = useCastChannel('urn:x-cast:com.url.cast', message => console.log('Received message', message));
+  
+  const channel = useCastChannel('urn:x-cast:com.url.cast');
   const client = useRemoteMediaClient();
+  const session = useCastSession()
 
+  
+  
   useEffect(() => {
     const loadReceiverUrl = async () => {
       try {
         if (client && channel) { // Check if both client and channel are available
-          //Initial Image before Scoring Page
-          // await client.loadMedia({
-          //   mediaInfo: {
-          //     contentUrl: 'https://sportyapp.co.za/assets/images/SportyAppLoading.png',
-          //   },
-          // });
-          //Send message to the channel
-          channel.sendMessage({hello: 'world'})
-          console.log('Channel', channel)
-          // channel.sendMessage({ type: 'iframe', url: ' https://sportyapp.co.za/scoreapi.php?view=tv&target=0a897bf3-377e-5bfd-8f4e-a70bdf23cdf7.local' });
+          if (client && channel.sendMessage) {
+            const msg = {
+              type: 'iframe',
+              url: 'http://example.com/'
+            }
+
+              channel.sendMessage(msg)
+              console.log('Sent message')
+
+            console.log(channel)
+            console.log('Message sent');
+          } else {
+            console.error('sendMessage method is not available on the channel object.');
+          }
         }
       } catch (error) {
         console.error('Error loading custom receiver URL:', error);
       }
+
+      if(session && client){
+        session.addChannel('urn:x-cast:com.url.cast')
+        console.log('Session',session)
+      }
+
     };
 
     loadReceiverUrl();
     
     return () => {
-      
       // Clean up any resources if needed
     };
   }, [client, channel]);
